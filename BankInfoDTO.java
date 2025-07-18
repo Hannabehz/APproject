fce897360da08196557bcd68c7f6a5dc0e1fad05
Hannabehz <hannabeh94@gmail.com>
2025-07-18 15:41:37 +0330

    Add files via upload

--- BankInfoDTO.java
+++ BankInfoDTO.java
@@ -1,23 +1,20 @@
-package dto;
+package model;
 
 import com.google.gson.annotations.SerializedName;
 
 public class BankInfoDTO {
     @SerializedName("bankName")
     private String bankName;
-
     @SerializedName("accountNumber")
     private String accountNumber;
 
-    public String getBankName() { return bankName; }
-    public void setBankName(String bankName) { this.bankName = bankName; }
-
-    public String getAccountNumber() { return accountNumber; }
-    public void setAccountNumber(String accountNumber) { this.accountNumber = accountNumber; }
-    public BankInfoDTO() {}
     public BankInfoDTO(String bankName, String accountNumber) {
         this.bankName = bankName;
         this.accountNumber = accountNumber;
-
     }
+
+    public String getBankName() { return bankName; }
+    public void setBankName(String bankName) { this.bankName = bankName; }
+    public String getAccountNumber() { return accountNumber; }
+    public void setAccountNumber(String accountNumber) { this.accountNumber = accountNumber; }
 }
